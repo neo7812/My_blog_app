6f5aea5c426aef05dee0d88a328fe429ad53be44
Saurav <saurabhu294@gmail.com>
2025-03-28 11:56:14 +0530

    fix view file

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 
 mongoose
   .connect(process.env.MONGO_URI, {
